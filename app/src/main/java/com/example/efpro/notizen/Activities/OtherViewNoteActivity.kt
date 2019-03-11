e02027932cc5d322c7db2511fd1257c6bc773a6d
suulcoder <ef.proyectomundial@gmail.com>
2019-03-10 19:18:04 -0600

    fixing a bug

--- app/src/main/java/com/example/efpro/notizen/Activities/OtherViewNoteActivity.kt
+++ app/src/main/java/com/example/efpro/notizen/Activities/OtherViewNoteActivity.kt
@@ -3,6 +3,11 @@ package com.example.efpro.notizen.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.efpro.notizen.R
+import com.example.efpro.notizen.models.Nota
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_other_view_note.*
 
 class OtherViewNoteActivity : AppCompatActivity() {
@@ -22,10 +27,26 @@ class OtherViewNoteActivity : AppCompatActivity() {
         }
 
         tittle.text = titulo
-        correo.text = mail
         descripcion.text = description
         content.text = contenido
+        val reference = FirebaseDatabase.getInstance().getReference("users")
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+            override fun onDataChange(p0: DataSnapshot) {
+                val nota =p0.getValue() as HashMap<*, *>
+                val it = nota.keys.iterator()//We iterate the hash
+                while(it.hasNext()){
+                    val key = it.next()
+                    val currentNote = nota.get(key) as HashMap<*,*>
+                    if(currentNote.get("id").toString() == mail){
+                        correo.text = currentNote.get("nombre").toString() + " | " + currentNote.get("e mail").toString()
+                    }
+                }
+            }
 
+        })
 
     }
 }
